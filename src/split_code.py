02199b4c11905f71204d065c02bf5dd0f5060064
Mossblac <Murphyrmurphy2000@gmail.com>
2025-03-13 20:58:23 -0600

    split_code complete and tested

--- src/split_code.py
+++ src/split_code.py
@@ -1,8 +1,42 @@
-from textnode import TextType
+from textnode import TextType, TextNode
 
 
 
-def split_nodes_delimeter(old_nodes, delimeter, text_type):
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type is not TextType.TEXT:
+            new_nodes.append(node)
+        else:
+            text = node.text
+            if delimiter in text:
+                start_index = text.find(delimiter)
+                end_index = text.find(delimiter, start_index + len(delimiter))
+                if end_index == -1:
+                    raise Exception("No closing delimiter found")
+                
+                before_text = text[:start_index]
+                
+                between_text = text[start_index + len(delimiter):end_index]
+            
+                after_text = text[end_index + len(delimiter):]
+                 
+                if before_text:
+                    new_nodes.append(TextNode(before_text, TextType.TEXT))
+                new_nodes.append(TextNode(between_text, text_type))
+                if after_text:
+                    temp_node = TextNode(after_text, TextType.TEXT)
+                    result = split_nodes_delimiter([temp_node], delimiter, text_type)
+                    new_nodes.extend(result)
+            else:
+                new_nodes.append(node)
+    
+    return new_nodes
+
+               
+
+
+
 
 
 
---
+++ src/test_split_code.py
@@ -0,0 +1,54 @@
+import unittest
+from textnode import TextNode, TextType
+from htmlnode import HTMLNode, ParentNode, LeafNode
+from split_code import split_nodes_delimiter
+
+
+class Test_Split_Code(unittest.TestCase):
+    def test_delimeter(self):
+        node = TextNode("This is a sentence with a **bold** word in it", TextType.TEXT)
+        new_node = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(new_node, [
+            TextNode("This is a sentence with a ", TextType.TEXT),
+            TextNode("bold", TextType.BOLD),
+            TextNode(" word in it", TextType.TEXT)
+
+        ])
+
+    def test_multiple_del(self):
+        node = TextNode("multiple _words_ are very _italic_ in this sentence", TextType.TEXT)
+        new_node = split_nodes_delimiter([node], "_", TextType.ITALIC)
+        self.assertEqual(new_node, [
+            TextNode("multiple ", TextType.TEXT),
+            TextNode("words", TextType.ITALIC),
+            TextNode(" are very ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" in this sentence", TextType.TEXT)
+
+        ])
+
+    def test_recursion(self):
+        node = TextNode("this `text` is `actually` very `annoying` to `test`", TextType.TEXT)
+        new_node = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(new_node, [
+            TextNode("this ", TextType.TEXT),
+            TextNode("text", TextType.CODE),
+            TextNode(" is ", TextType.TEXT),
+            TextNode("actually", TextType.CODE), 
+            TextNode(" very ", TextType.TEXT),
+            TextNode("annoying", TextType.CODE),
+            TextNode(" to ", TextType.TEXT),
+            TextNode("test", TextType.CODE)
+
+        ])
+
+
+    def test_no_close_error(self):
+        node = TextNode("this **bold is missing something", TextType.TEXT)
+        with self.assertRaises(Exception):
+            split_nodes_delimiter([node], "**", TextType.BOLD)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
