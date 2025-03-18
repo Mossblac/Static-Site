f297ef125542255544cf6b9a096288a54bc979ea
Mossblac <Murphyrmurphy2000@gmail.com>
2025-03-18 08:41:26 -0600

    updated all files to solutions for study

--- src/split_code.py
+++ src/split_code.py
@@ -2,6 +2,15 @@ import re
 from textnode import TextType, TextNode
 from regex_code import extract_markdown_images, extract_markdown_links
 
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
+
 
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
@@ -87,30 +96,6 @@ def split_nodes_link(old_nodes):
         if original_text != "":
             new_nodes.append(TextNode(original_text, TextType.TEXT))
     return new_nodes
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
