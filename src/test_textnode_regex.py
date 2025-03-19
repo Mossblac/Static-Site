32cf4ac4bbbf7ebb77ab21b7a8fb643f708e3cb1
Mossblac <Murphyrmurphy2000@gmail.com>
2025-03-18 22:17:57 -0600

    matched to solution files

--- src/test_textnode_regex.py
+++ src/test_textnode_regex.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 from regex_code import extract_markdown_images, extract_markdown_links
 
 
@@ -35,6 +35,52 @@ class TestRegex(unittest.TestCase):
         "This is (text) with an ![image](https://i.imgur.com/zjjcJKZ.png)"
     )
         self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+
+
+class TestMain(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold(self):
+        node1 = TextNode("this is bold", TextType.BOLD)
+        html_node1 = text_node_to_html_node(node1)
+        self.assertEqual(html_node1.tag, "b")
+        self.assertEqual(html_node1.value, "this is bold")
+        
+    def test_exception(self):
+        node = TextNode("some text", "highlight")
+        with self.assertRaises(Exception):
+            text_node_to_html_node(node)
+            
+    def test_italic(self):
+        node = TextNode("this is italic", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "this is italic")
+
+    def test_code(self):
+        node = TextNode("this is code", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "this is code")
+
+    def test_link(self):
+        node = TextNode("click me", TextType.LINK, "http//:www.google.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "click me")
+        self.assertEqual(html_node.props, {'href':"http//:www.google.com"})
+
+    def test_image(self):
+        node = TextNode("alt text", TextType.IMAGE, "/image.jpeg")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.props, {"src": "/image.jpeg", "alt": "alt text"})
         
 
 
