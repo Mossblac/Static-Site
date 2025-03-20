2b9ed9da8264bcc60cb2e6c7ce13f59027f87865
Mossblac <Murphyrmurphy2000@gmail.com>
2025-03-20 15:26:19 -0600

    main site is generating, sub-site info in place

--- src/main.py
+++ src/main.py
@@ -1,12 +1,42 @@
-from textnode import TextNode, TextType
+import shutil
+import os
+from md_to_html import generate_page
+
+def copy_recursive(source, destination):
+    # Create the destination directory if it doesn't exist
+    if not os.path.exists(destination):
+        os.mkdir(destination)
+
+    # Process each item in the source directory
+    for item in os.listdir(source):
+        source_path = os.path.join(source, item)
+        dest_path = os.path.join(destination, item)
+
+        if os.path.isfile(source_path):  # If it's a file, copy it
+            shutil.copy(source_path, dest_path)
+        elif os.path.isdir(source_path):  # If it's a folder, recurse!
+            copy_recursive(source_path, dest_path)
 
 
 def main():
-    
-    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-    print(node)
+    source = "static"
+    destination = "public"
 
-    
+    # Delete "public" directory, if it exists
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+
+    # Recreate the empty "public" directory
+    os.mkdir(destination)
+
+    # Call our recursive function to copy everything
+    copy_recursive(source, destination)
+
+    from_path = "content/index.md"
+    template = "template.html"
+    dest_path = "public/index.html"
+
+    return generate_page(from_path, template, dest_path)
 
    
 
---
+++ src/md_to_html.py
@@ -0,0 +1,136 @@
+import os
+import shutil
+from textnode import TextType, TextNode, text_node_to_html_node
+from htmlnode import ParentNode
+from split_code import (
+    text_to_textnodes,
+    markdown_to_blocks,
+    block_to_block_type,
+    BlockType
+)
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    with open(from_path) as file:
+        from_path_content = file.read()
+    with open(template_path) as file:
+        template_path_content = file.read()
+    from_path_node = markdown_to_html_node(from_path_content)
+    from_path_html = from_path_node.to_html()
+    title = extract_title(from_path_content)
+    final_html = template_path_content.replace("Title", f"{title}").replace(f"Content", f"{from_path_html}")
+    path = os.path.dirname(dest_path)
+    if not os.path.exists(path):
+        os.makedirs(path)
+    file_destination = os.path.join(path, "index.html")
+    with open(file_destination, 'w') as f:
+        f.write(final_html)
+
+
+def extract_title(markdown):
+    title = ""
+    markdown_split = markdown.split("\n\n")
+    if "# " not in markdown_split[0]:
+        raise Exception("no title")
+    for line in markdown_split:
+        if line.startswith("# "):
+            title += line.strip("#")
+    return title.strip()
+
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    children = []
+    for block in blocks:
+        html_node = block_to_html_node(block)
+        children.append(html_node)
+    return ParentNode("div", children, None)
+
+
+def block_to_html_node(block):
+    block_type = block_to_block_type(block)
+    if block_type == BlockType.PARAGRAPH:
+        return paragraph_to_html_node(block)
+    if block_type == BlockType.HEADING:
+        return heading_to_html_node(block)
+    if block_type == BlockType.CODE:
+        return code_to_html_node(block)
+    if block_type == BlockType.ORDERED_LIST:
+        return olist_to_html_node(block)
+    if block_type == BlockType.UNORDERED_LIST:
+        return ulist_to_html_node(block)
+    if block_type == BlockType.QUOTE:
+        return quote_to_html_node(block)
+    raise ValueError("invalid block type")
+
+
+def text_to_children(text):
+    text_nodes = text_to_textnodes(text)
+    children = []
+    for text_node in text_nodes:
+        html_node = text_node_to_html_node(text_node)
+        children.append(html_node)
+    return children
+
+
+def paragraph_to_html_node(block):
+    lines = block.split("\n")
+    paragraph = " ".join(lines)
+    children = text_to_children(paragraph)
+    return ParentNode("p", children)
+
+
+def heading_to_html_node(block):
+    level = 0
+    for char in block:
+        if char == "#":
+            level += 1
+        else:
+            break
+    if level + 1 >= len(block):
+        raise ValueError(f"invalid heading level: {level}")
+    text = block[level + 1 :]
+    children = text_to_children(text)
+    return ParentNode(f"h{level}", children)
+
+
+def code_to_html_node(block):
+    if not block.startswith("```") or not block.endswith("```"):
+        raise ValueError("invalid code block")
+    text = block[4:-3]
+    raw_text_node = TextNode(text, TextType.TEXT)
+    child = text_node_to_html_node(raw_text_node)
+    code = ParentNode("code", [child])
+    return ParentNode("pre", [code])
+
+
+def olist_to_html_node(block):
+    items = block.split("\n")
+    html_items = []
+    for item in items:
+        text = item[3:]
+        children = text_to_children(text)
+        html_items.append(ParentNode("li", children))
+    return ParentNode("ol", html_items)
+
+
+def ulist_to_html_node(block):
+    items = block.split("\n")
+    html_items = []
+    for item in items:
+        text = item[2:]
+        children = text_to_children(text)
+        html_items.append(ParentNode("li", children))
+    return ParentNode("ul", html_items)
+
+
+def quote_to_html_node(block):
+    lines = block.split("\n")
+    new_lines = []
+    for line in lines:
+        if not line.startswith(">"):
+            raise ValueError("invalid quote block")
+        new_lines.append(line.lstrip(">").strip())
+    content = " ".join(new_lines)
+    children = text_to_children(content)
+    return ParentNode("blockquote", children)
