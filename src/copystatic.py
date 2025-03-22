aacb7d6cf3043c3dd6c3ddb0ec9a11019a1a0dee
Mossblac <Murphyrmurphy2000@gmail.com>
2025-03-22 11:19:35 -0600

    this should be working now, probably not though, second try

--- src/copystatic.py
+++ src/copystatic.py
@@ -1,16 +1,18 @@
 import os
 import shutil
 
-
 def copy_files_recursive(source_dir_path, dest_dir_path):
+    # Create the destination directory and all its parents, if it doesn't exist
     if not os.path.exists(dest_dir_path):
-        os.mkdir(dest_dir_path)
+        os.makedirs(dest_dir_path)
 
+    # Iterate through the contents of the source directory
     for filename in os.listdir(source_dir_path):
-        from_path = os.path.join(source_dir_path, filename)
-        dest_path = os.path.join(dest_dir_path, filename)
-        print(f" * {from_path} -> {dest_path}")
-        if os.path.isfile(from_path):
+        from_path = os.path.join(source_dir_path, filename)  # Full path in source
+        dest_path = os.path.join(dest_dir_path, filename)    # Full path in target
+        print(f" * {from_path} -> {dest_path}")  # Debugging log
+
+        if os.path.isfile(from_path):  # Copy the file if it is a standard file
             shutil.copy(from_path, dest_path)
-        else:
+        elif os.path.isdir(from_path):  # Recursively copy directories
             copy_files_recursive(from_path, dest_path)
